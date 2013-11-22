685db25bdd48094578bfa15ebaab1d8df1a74ca1
Ruben <ruben.perez@alu.uclm.es>
2013-11-22 17:51:54 +0100

    delete all

--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-icegridadmin -u nobody -p nopass -e "node shutdown Nodo3"     --Ice.Config="config/locator.cfg"
-icegridadmin -u nobody -p nopass -e "node shutdown Nodo2"     --Ice.Config="config/locator.cfg"
-icegridadmin -u nobody -p nopass -e "node shutdown Nodo1"     --Ice.Config="config/locator.cfg"
